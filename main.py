9fd7c1a53b37231f39f0f2a7c4349001e3c1f0f6
Фёдор Ведерников <fyavedernikov@gmail.com>
2025-12-03 05:31:34 +0300

    запуск ( тест ) линии

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from env import LineEnv
+from ui import MainWindow
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    env = LineEnv(length=4)
+    window = MainWindow(env)
+    window.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
